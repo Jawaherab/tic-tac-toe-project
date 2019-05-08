2788ce98798334c81a4c645f6f739c76f4790777
JawaherAb <jawaheralabdullah0@gmail.com>
2019-05-08 22:48:48 +0300

    Tic Tac Toe

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# tic-tac-toe-project
+
+#Welcome to the TicTacToe Game , a simple Javascript web application allowing users to interact with the game,
+# The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game .
+
+#The goal of this project is to develop a responsive website of Tic-Tac-Toe game  and allows multi-player
+# to choose whether to play as (X or O) or plays against the computer.
+
+
+
+
+
+List technologies used
+HTML
+CSS
+jquery
+JavaScript{
+If statement
+forEach() method
+object
+While statement
+}
+Sweet alert
+https://sweetalert.js.org/
+The prompt() method 
+Animate css
+https://daneden.github.io/animate.css/?
+
+
+Link to wireframes and user stories.
+
+As a user, you able to start a new tic tac toe game
+As a user, you able to click on a square to add X first and then O, and so on
+As a user, you show a message after each turn for if I win, lose, tie or who's turn it is next
+As a user, you not able to click the same square twice
+As a user, you show a message when you win, lose or tie
+As a user, you not able to continue playing once you win, lose, or tie
+As a user, you able to play the game again without refreshing the page
+
+As a user, you able to  track of multiple game rounds with a win, lose and tie counter
+As a user, you can choose your tokens (X, O)
+As a user, you can play the Audio in your game
+As a user, you able to play a game against computer
+As a user, you able to  playable from a mobile phone with a responsive web design
+ Creative design with hover effects and animations
+
+ 
+
+Document your planning and tell a story about your development process and problem-solving strategy.
+
+
+How to play
+Players selection
+You need to insert the player that goes first X or O players.
+
+Human versus Human.
+If two human players start to play, you just need to
+ click in an empty position in the grid to display (X or O).  
+
+Human versus Computer.
+click on button ,If you want to play with computer ,  If it is your turn, 
+just click in the position you want to play in the grid. After that the computer will perform its move. 
+
+ win counter:
+ if a players win , their score  increase
+
+Game over:
+If a player forms a row, a column or a diagonal with same letter player wins and the game ends. 
+If the grid is full and there’s no row, column or diagonal of the same letter,the game ends at draw. 
+
+Once the game has a winner or a draw, you will see a dialog informing about the result. 
+if you want to play again just click on reset button .
+
+
+
+List unsolved problems which would be fixed in future iterations :
+
+When play with a computer, the score of winner increase twice.
+
+
+
+Describe how you solved for the winner :
+
+I used forEach() method executes a provided function once each element of an a correctValue array.
+the function checks if  row or vertical line or  diagonal have a same letter ("XXX","OOO")
+you will see a dialog informing about whose the winner, and the counter of score increase automaticly
+
+
+
+It was fun and very challenging to me as this is the first time to build an game application
+
+
--- main.js
+++ main.js
@@ -14,62 +14,82 @@
 
 //turn of player
 
+let computer = false;
 
- let players= {
+
+let players= {
     player1: " ",
     player2: " ",
     numberPlay:1,
     scorePlayer1:0,
-    scorePlayer2:0
-    };
-   
-   
+    scorePlayer2:0,
+    
+};
+//the result of the game
+let playerscore= 0;
+const sound = document.getElementById("audio");
+
 const tokens=function(event){
     let input = prompt("You have to insert 'X' or 'O' ")
-  if ( input.toUpperCase() === "X"){
-      players.player1 = "X";
-      players.player2 = "O";
-}else if (input.toUpperCase()  === "O"){
+    if ( input.toUpperCase() === "X"){
+        players.player1 = "X";
+        players.player2 = "O";
+    }else if (input.toUpperCase()  === "O"){
       
-      players.player1  = "O";
-      players.player2  = "X";
+        players.player1  = "O";
+        players.player2  = "X";
+    }
 }
-}
-tokens();    
-
- 
-
-
-
-
+  
     // 1,3,5,7,9 => X
     // 2,4, 6, 8 , =>o
-    let turn = function(event){
-
-
+let turn = function(event){
+    if(computer === false){
         if (players.numberPlay %2 === 0 ){
             $(event.target).text(players.player2)
             $("p").text("Next player " + players.player1)  // //who’s turn is it
-
+    
+    
         }else {
             $(event.target).text(players.player1)
             $("p").text("Next player " + players.player2)
             
         }
         $(event.target).off("click")
-        findWinner();
+        
+       
         console.log(players.numberPlay);
         players.numberPlay += 1;
-        
+    }else{
+        computer = players.player2;
+        console.log(players.numberPlay);
+        //computer 
+        if (players.numberPlay %2 === 0 ){
+            let random = Math.floor((Math.random() * 9) + 1);
+            while($("#button"+ random).text() !== ""){
+                random = Math.floor((Math.random() * 9) + 1);
+             
+            }
+           
+            $("#button"+ random).text(computer)
+            $("p").text("Next player " + players.player1)  // //who’s turn is it
+        }else{ //person
+            $(event.target).text(players.player1)
+            $("p").text("Next player " + computer)  // //who’s turn is it
+            $(event.target).off("click")
+            if(players.gameOver !== true){
+                setTimeout(turn,1000); //1000ms = 1s
+            }
+         
         }
-
-        $('.position').on("click",turn);
-
-
-
-//the result of the game
-let playerscore= 0;
-
+        players.numberPlay += 1;
+        
+        
+    }
+    findWinner();
+    
+    
+}
 
 const findWinner =function(){
 
@@ -88,57 +108,50 @@ const findWinner =function(){
                 $('.position').off("click");
                 players.scorePlayer1 += 1
                 $('#count1').html(players.scorePlayer1);
+                
             }else{
                 swal("Good job O !", "You Winner!", "success");
                   $('.position').off("click");
                   players.scorePlayer2 += 1
                  $('#count2').html(players.scorePlayer2);
+                 
             }      
-   } else if(players.numberPlay === 9 ){
-    swal(" DRAW !");
-}
+        } else if(players.numberPlay === 10 ){
+            swal(" DRAW !");
+        }
 
 
     });
-
-
 }
 
 
+tokens();  
+$('.position').on("click",turn);
+
 //play again function
-   
-    $("#reset").on("click", function(event){
-        $('.position').on("click",turn);
-        players.numberPlay = 1;
-        $(".position").text("");
-        
-
+$("#reset").on("click", function(event){
+    $(".position").off("click");
+    $('.position').on("click",turn);
+    players.numberPlay = 1;
+    $(".position").text(""); 
 });
-
-    
-
-    
-
-// play with computer
-let random = Math.floor((Math.random() * 9) + 1);
-let r;
-$(".com").on("click" , function(event){
-
-    if ( random % 2 === 0){
-        players.numberPlay = random;
-        console.log("lll");
-    }
-})
-
-
-
 //sound of click
-
-var sound = document.getElementById("audio");
-
 $('.row').click(function(event){
   event.preventDefault();
   sound.play();
 });
+
+
+
+//play with computer
+
+$(".com").on("click" , function(event ){
+    
+    computer = true;
+     
+})
+
+
+
 
 
